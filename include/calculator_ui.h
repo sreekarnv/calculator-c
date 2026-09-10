6f370e02c27e84e2498e5a6b744f24236852020b
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-10 17:38:56 +0530

    :wrench: chore: setup and verify gtk

---
+++ include/calculator_ui.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <gtk/gtk.h>
+
+static void print_hello(GtkWidget *widget, gpointer data)
+{
+    g_print("Hello World\n");
+}
+
+static void calculator_activate(GtkApplication *app, gpointer user_data)
+{
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Calculator");
+    gtk_window_set_default_size(GTK_WINDOW(window), 380, 650);
+
+    button = gtk_button_new_with_label("Hello World");
+    gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    gtk_window_set_child(GTK_WINDOW(window), button);
+
+    gtk_window_present(GTK_WINDOW(window));
+}
--- src/main.c
+++ src/main.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include "calculator_expr.h"
+#include "calculator_ui.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-    int a[] = {10, 20};
-    int sum_;
-    add(a, 2, &sum_);
+    // int a[] = {10, 20};
+    // int sum_;
+    // add(a, 2, &sum_);
 
-    int diff_;
-    subtract(a, 2, &diff_);
+    // int diff_;
+    // subtract(a, 2, &diff_);
 
-    int mul_;
-    multiply(a, 2, &mul_);
+    // int mul_;
+    // multiply(a, 2, &mul_);
 
-    double div_;
-    divide(a, 2, &div_);
+    // double div_;
+    // divide(a, 2, &div_);
 
-    int mod_;
-    modulus(a, 2, &mod_);
+    // int mod_;
+    // modulus(a, 2, &mod_);
 
-    if (sum_ && diff_ && mul_ && div_)
-    {
-        printf("Sum = %d\n", sum_);
-        printf("Diff = %d\n", diff_);
-        printf("Mul = %d\n", mul_);
-        printf("Div = %.2lf\n", div_);
-        printf("Mod = %d\n", mod_);
-    }
+    // if (sum_ && diff_ && mul_ && div_)
+    // {
+    //     printf("Sum = %d\n", sum_);
+    //     printf("Diff = %d\n", diff_);
+    //     printf("Mul = %d\n", mul_);
+    //     printf("Div = %.2lf\n", div_);
+    //     printf("Mod = %d\n", mod_);
+    // }
 
-    return 0;
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(calculator_activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
